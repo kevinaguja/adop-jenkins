484d76cc401749b1b61ca3aa8cdc8e891113b6ba
bzon <bryansazon@hotmail.com>
2017-09-16 07:47:05 +0000

    gitlab-ext-2 development

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,6 @@
-FROM jenkins:2.7.2
+FROM jenkins/jenkins:2.60.3
 
-MAINTAINER Bryan Sazon and Arcy Teodoro 
-
-ENV GERRIT_HOST_NAME gerrit
-ENV GERRIT_PORT 8080
-ENV GERRIT_JENKINS_USERNAME="" GERRIT_JENKINS_PASSWORD=""
-ENV GITLAB_HOST_NAME gitlab
-ENV GITLAB_PORT 80
-ENV GITLAB_JENKINS_USERNAME="" GITLAB_JENKINS_PASSWORD="" GITLAB_JENKINS_TOKEN=""
-ENV GIT_REPO gitlab
+MAINTAINER John Bryan Sazon and Arcy Teodoro 
 
 # Copy in configuration files
 COPY resources/plugins.txt /usr/share/jenkins/ref/
@@ -20,17 +12,45 @@ COPY resources/views/ /usr/share/jenkins/ref/init.groovy.d/
 COPY resources/m2/ /usr/share/jenkins/ref/.m2
 COPY resources/entrypoint.sh /entrypoint.sh
 COPY resources/jenkins_home/ /usr/share/jenkins/ref/
+COPY resources/plugins-v2.sh /usr/local/bin/plugins-v2.sh
 
 # Reprotect
 USER root
 RUN chmod +x -R /usr/share/jenkins/ref/adop_scripts/ && chmod +x /entrypoint.sh
+
+# Install additional packages
+# zip is required for plugins-v2.sh
+RUN wget "http://http.us.debian.org/debian/pool/main/z/zip/zip_3.0-11+b1_amd64.deb" && \
+    dpkg -i zip_3.0-11+b1_amd64.deb && \
+    rm -f zip_3.0-11+b1_amd64.deb
+
 # USER jenkins
 
-# Environment variables
-ENV ADOP_LDAP_ENABLED=true ADOP_SONAR_ENABLED=true ADOP_ANT_ENABLED=true ADOP_MAVEN_ENABLED=true ADOP_NODEJS_ENABLED=true ADOP_GERRIT_ENABLED=true
-ENV LDAP_GROUP_NAME_ADMIN=""
+# Environment variables for SCM
+ENV GERRIT_HOST_NAME=gerrit\
+    GERRIT_PORT=8080 \
+    GERRIT_JENKINS_USERNAME="" \
+    GERRIT_JENKINS_PASSWORD="" \
+    GITLAB_HOST_NAME=gitlab \
+    GITLAB_HTTP_URL=http://${GITLAB_HOST_NAME}/gitlab \
+    GITLAB_PORT=80 \
+    GITLAB_JENKINS_USERNAME="" \
+    GITLAB_JENKINS_PASSWORD="" \
+    GITLAB_JENKINS_TOKEN="" \
+    GIT_REPO=gitlab
+
+# Environment variables for Plugins switch
+ENV ADOP_LDAP_ENABLED=true \
+    ADOP_SONAR_ENABLED=true \
+    ADOP_ANT_ENABLED=true \
+    ADOP_MAVEN_ENABLED=true \
+    ADOP_NODEJS_ENABLED=true \
+    ADOP_GERRIT_ENABLED=false \
+    ADOP_GITLAB_ENABLED=false
+
 ENV JENKINS_OPTS="--prefix=/jenkins -Djenkins.install.runSetupWizard=false"
 
-RUN /usr/local/bin/plugins.sh /usr/share/jenkins/ref/plugins.txt
+#RUN /usr/local/bin/plugins.sh /usr/share/jenkins/ref/plugins.txt
+RUN /usr/local/bin/plugins-v2.sh /usr/share/jenkins/ref/plugins.txt
 
 ENTRYPOINT ["/entrypoint.sh"]
--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
+# Load secrets from files in $SECRETS_DIR
+# Secret format is KEY=VALUE encoded in base 64
+SECRETS_DIR=/etc/secrets
+if [[ -d $SECRETS_DIR ]]; then
+  COMMAND=$1; shift
+  ARGS=$@
+
+  export_vars() {
+    while IFS= read -r LINE || [[ -n "$LINE" ]]; do
+      export $(echo $LINE)
+    done < $1
+  }
+
+  recurse_dir() {
+    if [ "$(ls -A $1)" ]; then
+      local SECRET
+      for SECRET in $1/*; do
+        if [ -d $SECRET ]; then
+          recurse_dir $SECRET
+        else
+          export_vars $SECRET
+        fi
+      done
+    fi
+  }
+
+  if  [ -d $SECRETS_DIR ]; then
+    recurse_dir $SECRETS_DIR
+  fi
+fi
+
 if [[ $ADOP_GERRIT_ENABLED == "true" ]] && [[ $ADOP_GITLAB_ENABLED = "true" ]]; then
 
   echo "You can't have both Gerrit and Gitlab enabled.."
@@ -11,8 +42,8 @@ elif [[ $ADOP_GERRIT_ENABLED == "true" ]]; then
   echo "'jenkins' user will now be configured for Gerrit."
   host=$GERRIT_HOST_NAME
   port=$GERRIT_PORT
-  username=$GERRIT_JENKINS_USERNAME
-  password=$GERRIT_JENKINS_PASSWORD
+  username=$JENKINS_USERNAME
+  password=$JENKINS_PASSWORD
 
   # Delete Load Platform for Gitlab
   rm -rf /usr/share/jenkins/ref/jobs/GitLab_Load_Platform
@@ -26,19 +57,19 @@ elif [[ $ADOP_GITLAB_ENABLED = "true" ]]; then
 
   # Generate SSH key
   echo "'jenkins' user will now be configured for Gitlab."
-  host=$GITLAB_HOST_NAME
-  port=$GITLAB_PORT
-  username=$GITLAB_JENKINS_USERNAME
-  password=$GITLAB_JENKINS_PASSWORD
+  host=${GITLAB_HOST_NAME:-gitlab}
+  port=${GITLAB_PORT:-22}
+  username=${JENKINS_USERNAME}
+  password=${JENKINS_PASSWORD}
   nohup /usr/share/jenkins/ref/adop\_scripts/generate_key.sh -c ${host} -p ${port} -u ${username} -w ${password} &
 
   # Wait until gitlab is up and running
   SLEEP_TIME=10
   MAX_RETRY=12
   COUNT=0
-  until [[ $(curl -I -s gitlab/gitlab/users/sign_in | head -1 | grep 200 | wc -l) -eq 1 ]] || [[ $COUNT -eq $MAX_RETRY ]]
+  until [[ $(curl -I -s ${GITLAB_HTTP_URL}/users/sign_in | head -1 | grep 200 | wc -l) -eq 1 ]] || [[ $COUNT -eq $MAX_RETRY ]]
   do
-    echo "Testing GitLab Connection endpoint - http://gitlab/gitlab .."
+    echo "Testing GitLab Connection endpoint - ${GITLAB_HTTP_URL} .."
     echo "GitLab unavailable, sleeping for ${SLEEP_TIME}s ..retrying $COUNT/$MAX_RETRY"
     sleep ${SLEEP_TIME}
     ((COUNT ++))
@@ -47,30 +78,28 @@ elif [[ $ADOP_GITLAB_ENABLED = "true" ]]; then
   if [[ $COUNT -eq $MAX_RETRY ]]
   # Skip Jenkins and Gitlab key configuration
   then
-    echo "Couldn't wait for Gitlab anymore. SSH and Jenkins gitlab token may not work properly.."
+    echo "Couldn't wait for ${GITLAB_HOST_NAME} anymore. SSH and Jenkins gitlab token may not work properly.."
 
   # Start 1 time configuration
   else
-    # Create token file for adop_gitlab.groovy to read
-    echo "Obtaining GitLab root token.."
-    GITLAB_ROOT_TOKEN="$(curl -X POST "http://gitlab/gitlab/api/v3/session?login=root&password=${GITLAB_ROOT_PASSWORD}" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['private_token'];")"
+    echo "Get the gitlab token and place it in a file for adop_gitlab.groovy"
+    GITLAB_ROOT_TOKEN="$(curl -X POST "${GITLAB_HTTP_URL}/api/v3/session?login=root&password=${GITLAB_ROOT_PASSWORD}" | python -c "import json,sys;obj=json.load(sys.stdin);print obj['private_token'];")"
     echo "${GITLAB_ROOT_TOKEN}" > ${JENKINS_HOME}/gitlab-root-token
 
-    # Initialize a login for jenkins user in Gitlab
-    public_key_val=$(cat ${JENKINS_HOME}/.ssh/id_rsa.pub)
-    echo "Initializing jenkins user in Gitlab.."
-    curl --silent --header "PRIVATE-TOKEN: ${GITLAB_ROOT_TOKEN}" -X POST "http://gitlab/gitlab/api/v3/users?email=${GIT_GLOBAL_CONFIG_EMAIL}&name=jenkins&username=jenkins&password=${password}&provider=ldap&extern_uid=cn=jenkins,ou=people,${LDAP_ROOTDN}&admin=true&confirm=false" | true
+    echo "Perform a first login for jenkins user in Gitlab"
+    curl --silent --header "PRIVATE-TOKEN: ${GITLAB_ROOT_TOKEN}" -X POST "${GITLAB_HTTP_URL}/api/v3/users?email=${GIT_GLOBAL_CONFIG_EMAIL}&name=jenkins&username=jenkins&password=${password}&provider=ldap&extern_uid=cn=${username},ou=people,${LDAP_ROOTDN}&admin=true&confirm=false" | true
 
-    # Send ssh key to gitlab's root user profile
+    echo "Send ssh key to gitlab's root user profile"
+    public_key_val=$(cat ${JENKINS_HOME}/.ssh/id_rsa.pub)
     echo "Adding jenkins SSH key to GitLab root user.."
-    curl --silent --header "PRIVATE-TOKEN: ${GITLAB_ROOT_TOKEN}" -X POST "http://gitlab/gitlab/api/v3/users/1/keys" --data-urlencode "title=jenkins@adop-core" --data-urlencode "key=${public_key_val}" | true
+    curl --silent --header "PRIVATE-TOKEN: ${GITLAB_ROOT_TOKEN}" -X POST "${GITLAB_HTTP_URL}/api/v3/users/1/keys" --data-urlencode "title=jenkins@adop-core" --data-urlencode "key=${public_key_val}" | true
   fi
 
 fi
 
 echo "Starting Jenkins.."
 echo "skip upgrade wizard step after installation"
-echo "2.7.2" > /var/jenkins_home/jenkins.install.UpgradeWizard.state
+echo "2.60.3" > /var/jenkins_home/jenkins.install.UpgradeWizard.state
 
 chown -R 1000:1000 /var/jenkins_home
 su jenkins -c /usr/local/bin/jenkins.sh
